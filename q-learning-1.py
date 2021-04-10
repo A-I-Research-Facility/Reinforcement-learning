389958e927ac637c7290c4e80ebfb52cd31dfaf3
Nimesh Srivastava <snimesh412@gmail.com>
2021-04-10 13:58:53 +0530

    update

--- q-learning-1.py
+++ q-learning-1.py
@@ -1,20 +1,20 @@
 # The evironment used over here doesn't make a difference to our model, but it's easy to understand
 
-import gym
+import gym      # pip installation
 
 env = gym.make("MountainCar-v0")    # this creates an environment with a mountain, and a car
 env.reset()     # Resetting is the first thing to do after we creaate an environment
                 # Then we are ready to iterate through it
-done = False
+# done = False
 
-while not done:
-    action = 2      # This environment has three actions, 0 = push car left, 1 = do nothing, 2 = push car right
-    new_state, reward, done, _ = env.step(action)   # everytime we step through an action, we get a new_state from environment
-                                                    # For our sake of understanding, we know that the state returned by the 
-                                                    # environment is position and velocity.
-    env.render()    # rendering the GUI
+# while not done:
+#     action = 2      # This environment has three actions, 0 = push car left, 1 = do nothing, 2 = push car right
+#     new_state, reward, done, _ = env.step(action)   # everytime we step through an action, we get a new_state from environment
+#                                                     # For our sake of understanding, we know that the state returned by the 
+#                                                     # environment is position and velocity.
+#     env.render()    # rendering the GUI
 
-env.close()
+# env.close()
 
 # When we run this program, we see a car trying to climb the hill. But it isn't able to because it needs more momentum.
 # So, now we need to do that
@@ -26,4 +26,29 @@ env.close()
 # We initialise the Q-table with random values. So, first our agent explores and does random stuff, but slowly updates those
 # Q-values with time.
 
-# 
+# To check all observations and all possible actions, run following (only works in gym environments):
+print(env.observation_space.high)       # Output : [0.6     0.07]
+print(env.observation_space.low)        # Output : [-1.2     -0.07]
+print(env.action_space.n)               # Output : 3
+
+# We want our Q-table to be of managable size. However, hardcoding the size is not the right move, since a real
+# RL model would not have this hardcoded beacuse it will change with environment
+DISCRETE_OS_SIZE = [20] * len(env.observation_space.high) # 20 * the length of any random observation space thing = [20] * 2
+
+# We are trying to separate the range of observation into 20 discrete chunks. Now we need to know the size of
+# those chunks
+discrete_os_win_size = (env.observation_space.high-env.observation_space.low) / DISCRETE_OS_SIZE
+
+print(discrete_os_win_size)     # Output : [0.09     0.007]
+
+#       ______________________________________
+#      |Combinations/Actions|  0  |  1  |  2  |
+#      +--------------------+-----+-----+-----+
+#      |        C1          |  0  |  2  |  2  |     <- Q-values, the action with largest q-value is chosen
+#      +--------------------+-----+-----+-----+                  to be performed by the agent. Initially it doesn't 
+#      |        C2          |  0  |  1  |  2  |                  happen. But overtime the agent realises what to do.
+#      +--------------------+-----+-----+-----+
+#      |        C3          |  2  |  0  |  1  |
+#      +--------------------+-----+-----+-----+
+
+# Initialising the Q-table :
