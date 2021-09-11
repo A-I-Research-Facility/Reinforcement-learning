c9c4bd1b21dca66cb798d990fd2a92920de71b60
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-09-11 11:21:15 +0530

    Update ql-2.py

--- q-learning-2/ql-2.py
+++ q-learning-2/ql-2.py
@@ -1,52 +1,27 @@
-'''
-Progressing on what we developed in the previous program, this one deals with the 
-agent actually achieving its goal. 
-
-We create multiple episodes for our agent to learn. Also, use of epsilon and its functions
-are introduced here.
-'''
-
 import gym
 import numpy as np
 
 env = gym.make("MountainCar-v0")
 
-# Now we are going to add certain constants. Their use will be explained later.
 LEARNING_RATE = 0.1
-DISCOUNT = 0.95     # measure of how much we value future reward over current reward (>0, <1)
+DISCOUNT = 0.95
 EPISODES = 25000
 
 SHOW_EVERY = 2000
 
 DISCRETE_OS_SIZE = [20] * len(env.observation_space.high)
 discrete_os_win_size = (env.observation_space.high-env.observation_space.low) / DISCRETE_OS_SIZE
-
-'''
-Some models require some random actions to be taken to ge the desired result. For this, we need to define
-epsilon over here. Even though in this case, our model is able to achive the goal without requiring
-this varible. Also, the value of epsilon varies between 0 and 1 only.
-
-Epsilon basically helps the model explore in random directions. It is suprising what the model finds
-out sometimes.
-'''
-epsilon = 0.5       # the higher the epsilon, the more likely the model is to perform a random action
+epsilon = 0.5
 START_EPSILON_DECAYING = 1
 END_EPSILON_DECAYING = EPISODES // 2
 
-epsilon_decay_value = epsilon / (END_EPSILON_DECAYING - START_EPSILON_DECAYING) # Amount decayed by in each episode
+epsilon_decay_value = epsilon / (END_EPSILON_DECAYING - START_EPSILON_DECAYING)
 
 q_table = np.random.uniform(low = -2, high = 0, size = (DISCRETE_OS_SIZE + [env.action_space.n]))
 
-# We need to convert the continuous states to discrete states. For that, we need a helper function
 def get_discrete_state(state):
     discrete_state = (state - env.observation_space.low) / discrete_os_win_size
-    # We need this returned in tuple form. Hence, 
     return tuple(discrete_state.astype(np.int))
-
-'''
-Now we want to iterate over episodes. Since currently the model only runs one time
-and we want more than that.
-'''
 
 for episode in range(EPISODES):
     if episode % SHOW_EVERY == 0:
@@ -57,22 +32,11 @@ for episode in range(EPISODES):
 
     discrete_state = get_discrete_state(env.reset())
 
-    # print(discrete_state)       # Output : (7,  10) {could be anything}
-
-    '''
-    We can now lookup that discrete state in the Q-table, and find the maximum Q-value
-    print(np.argmax(q_table[discrete_state]))
-
-    Since now we are ready with our new discrete state, our model can take action, and start generating
-    new Q-table.
-    We now require the while loop from previous program, but instead of hardcoded values, we will use dynamic values
-    '''
-
     done = False
 
     while not done:
         if np.random.random() > epsilon:
-            action = np.argmax(q_table[discrete_state])     # we will have new discrete state soon
+            action = np.argmax(q_table[discrete_state])
         else:
             action = np.random.randint(0, env.action_space.n)
         new_state, reward, done, _ = env.step(action)
@@ -80,20 +44,13 @@ for episode in range(EPISODES):
         if render:
             env.render()
         
-        # The environment might be over already, but if it is not
         if not done:
-            max_future_q = np.max(q_table[new_discrete_state])      # we use np.max() instead of argmax() beacuse we will use 
-                                                                    # max_future_q in our new Q formula, so we want the Q-value
-                                                                    # instead of the argument.
-                                                                    # Slowly overtime, Q-value gets back propagated down the table
+            max_future_q = np.max(q_table[new_discrete_state])
 
-            # Finding the current Q-value
             current_q = q_table[discrete_state + (action, )]
 
-            # The new Q-formula
-            new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)    # The way Q-value back propagates is based
-                                                                                                            # on all the parameters of this formula
-            q_table[discrete_state + (action, )] = new_q        # updating the Q-table based on the newest Q-value
+            new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q) 
+            q_table[discrete_state + (action, )] = new_q
 
         elif new_state[0] >= env.goal_position:
             print(f"We made it on episode  {episode}")
