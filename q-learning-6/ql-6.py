15e72af1ba7623f26f5bc2ace908bb5cfb78579a
Nimesh Srivastava <nimeshsrivastava00@gmail.com>
2021-09-11 21:41:54 +0530

    Update ql-6.py

--- q-learning-6/ql-6.py
+++ q-learning-6/ql-6.py
@@ -1,1 +1,377 @@
+'''
+In continuation with the previous program, we are now going to train
+our DQN model.
+TensorFlow version = 2.4.1 used
 
+This program has a very high computational time and needs lots of cuda cores
+to be even a little efficient. Please ensure you have a gpu with atleast 8GB
+of dedicated video memory and atleast 16GB of DDR4 RAM before running this.
+'''
+
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Activation, Flatten
+from keras.callbacks import TensorBoard
+from keras.optimizers import Adam
+from collections import deque
+import time
+import numpy as np
+from tqdm import tqdm
+import random
+import os
+from PIL import Image as Img
+import cv2
+
+REPLAY_MEMORY_SIZE = 50_000
+MODEL_NAME = "256x2"
+MIN_REPLAY_MEMORY_SIZE = 1_000
+MINIBATCH_SIZE = 64     # batch size for training data
+DISCOUNT = 0.99
+UPDATE_TARGET_EVERY = 5
+MIN_REWARD = -200
+EPISODES = 20_000
+
+epsilon = 1
+EPSILON_DECAY = 0.99975
+MIN_EPSILON = 0.001
+
+AGGREGATE_STATS_EVERY = 100     # number of episodes to see stats = 100
+SHOW_PREVIEW = False
+
+'''
+Now we want to bring in our blob class and blob environment.
+'''
+class Blob:
+    def __init__(self, size):
+        self.size = size
+        self.x = np.random.randint(0, size)
+        self.y = np.random.randint(0, size)
+
+    def __str__(self):
+        return f"Blob ({self.x}, {self.y})"
+
+    def __sub__(self, other):
+        return (self.x-other.x, self.y-other.y)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def action(self, choice):
+        '''
+        Gives us 9 total movement options. (0,1,2,3,4,5,6,7,8)
+        So, now our agent doesn't need to take help of the boundaries
+        to go up-down or left-right.
+        '''
+        if choice == 0:
+            self.move(x=1, y=1)
+        elif choice == 1:
+            self.move(x=-1, y=-1)
+        elif choice == 2:
+            self.move(x=-1, y=1)
+        elif choice == 3:
+            self.move(x=1, y=-1)
+
+        elif choice == 4:
+            self.move(x=1, y=0)
+        elif choice == 5:
+            self.move(x=-1, y=0)
+
+        elif choice == 6:
+            self.move(x=0, y=1)
+        elif choice == 7:
+            self.move(x=0, y=-1)
+
+        elif choice == 8:
+            self.move(x=0, y=0)
+
+    def move(self, x = None, y = None):         # updated for TensorFlow 2.4.1
+        # If no value for x, move randomly
+        if x == None:
+            self.x += np.random.randint(-1, 2)
+        else:
+            self.x += x
+
+        # If no value for y, move randomly
+        if y == None:
+            self.y += np.random.randint(-1, 2)
+        else:
+            self.y += y
+
+        # If we are out of bounds, fix!
+        if self.x < 0:
+            self.x = 0
+        elif self.x > self.size-1:
+            self.x = self.size-1
+        if self.y < 0:
+            self.y = 0
+        elif self.y > self.size-1:
+            self.y = self.size-1
+
+
+class BlobEnv:
+    SIZE = 10
+    RETURN_IMAGES = True
+    MOVE_PENALTY = 1
+    ENEMY_PENALTY = 300
+    FOOD_REWARD = 25
+    OBSERVATION_SPACE_VALUES = (SIZE, SIZE, 3)  # 4
+    ACTION_SPACE_SIZE = 9
+    PLAYER_N = 1  # player key in dict
+    FOOD_N = 2  # food key in dict
+    ENEMY_N = 3  # enemy key in dict
+    # the dict! (colors)
+    d = {1: (255, 175, 0),
+         2: (0, 255, 0),
+         3: (0, 0, 255)}
+
+    def reset(self):
+        self.player = Blob(self.SIZE)
+        self.food = Blob(self.SIZE)
+        while self.food == self.player:
+            self.food = Blob(self.SIZE)
+        self.enemy = Blob(self.SIZE)
+        while self.enemy == self.player or self.enemy == self.food:
+            self.enemy = Blob(self.SIZE)
+
+        self.episode_step = 0
+
+        if self.RETURN_IMAGES:
+            observation = np.array(self.get_image())
+        else:
+            observation = (self.player-self.food) + (self.player-self.enemy)
+        return observation
+
+    def step(self, action):
+        self.episode_step += 1
+        self.player.action(action)
+        
+        '''
+        To make the food and enemy move :
+        self.enemy.move()
+        self.food.move()
+        '''
+
+        if self.RETURN_IMAGES:
+            new_observation = np.array(self.get_image())
+        else:
+            new_observation = (self.player-self.food) + (self.player-self.enemy)
+
+        if self.player == self.enemy:
+            reward = -self.ENEMY_PENALTY
+        elif self.player == self.food:
+            reward = self.FOOD_REWARD
+        else:
+            reward = -self.MOVE_PENALTY
+
+        done = False
+        if reward == self.FOOD_REWARD or reward == -self.ENEMY_PENALTY or self.episode_step >= 200:
+            done = True
+
+        return new_observation, reward, done
+
+    def render(self):
+        img = self.get_image()
+        img = img.resize((300, 300))  # resizing so we can see our agent in all its glory.
+        cv2.imshow("image", np.array(img))  # show it!
+        cv2.waitKey(1)
+
+    # FOR CNN #
+    def get_image(self):
+        env = np.zeros((self.SIZE, self.SIZE, 3), dtype=np.uint8)  # starts an rbg of our size
+        env[self.food.x][self.food.y] = self.d[self.FOOD_N]  # sets the food location tile to green color
+        env[self.enemy.x][self.enemy.y] = self.d[self.ENEMY_N]  # sets the enemy location to red
+        env[self.player.x][self.player.y] = self.d[self.PLAYER_N]  # sets the player tile to blue
+        img = Img.fromarray(env, 'RGB')  # reading to rgb. Apparently. Even tho color definitions are bgr.
+        return img
+
+
+env = BlobEnv()
+
+# For stats
+ep_rewards = [-200]
+
+# For more repetitive results
+random.seed(1)
+np.random.seed(1)
+tf.random.set_seed(1)
+
+# Create models folder
+if not os.path.isdir('models'):
+    os.makedirs('models')
+
+class ModifiedTensorBoard(TensorBoard):     # completely updated to work with TensorFlow 2.4.1
+                                            # Please do not change this class in any manner
+                                            # or the program may not work at all. If you face some
+                                            # issue regarding this class, please create a pull request.
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.step = 1
+        self.writer = tf.summary.create_file_writer(self.log_dir)
+        self._log_write_dir = self.log_dir
+
+    def set_model(self, model):
+        self.model = model
+
+        self._train_dir = os.path.join(self._log_write_dir, 'train')
+        self._train_step = self.model._train_counter
+
+        self._val_dir = os.path.join(self._log_write_dir, 'validation')
+        self._val_step = self.model._test_counter
+
+        self._should_write_train_graph = False
+
+    def on_epoch_end(self, epoch, logs=None):
+        self.update_stats(**logs)
+
+    def on_batch_end(self, batch, logs=None):
+        pass
+
+    def on_train_end(self, _):
+        pass
+
+    def update_stats(self, **stats):
+        with self.writer.as_default():
+            for key, value in stats.items():
+                tf.summary.scalar(key, value, step = self.step)
+                self.writer.flush()
+
+class DQNAgent:
+    def __init__(self):
+        self.model = self.create_model() 
+
+        self.target_model = self.create_model() 
+        self.target_model.set_weights(self.model.get_weights())
+
+        self.replay_memory = deque(maxlen = REPLAY_MEMORY_SIZE)
+
+        self.tensorboard = ModifiedTensorBoard(log_dir = f"logs/{MODEL_NAME}-{int(time.time())}")
+        self.target_update_counter = 0      
+
+    def create_model(self):
+        model = Sequential()
+        model.add(Conv2D(256, (3, 3), input_shape = env.OBSERVATION_SPACE_VALUES))
+        model.add(Activation("relu"))
+        model.add(MaxPooling2D(2, 2))
+        model.add(Dropout(0.2))
+
+        model.add(Conv2D(256, (3, 3)))
+        model.add(Activation("relu"))
+        model.add(MaxPooling2D(2, 2))
+        model.add(Dropout(0.2))
+
+        model.add(Flatten())
+        model.add(Dense(64))
+
+        model.add(Dense(env.ACTION_SPACE_SIZE, activation = "linear"))
+        model.compile(loss="mse", optimizer = Adam(lr=0.001), metrics=['accuracy'])
+
+        return model
+    
+    def update_replay_memory(self, transition):
+        self.replay_memory.append(transition)
+
+    def get_qs(self, state):
+        return self.model.predict(np.array(state).reshape(-1, *state.shape) / 255)[0]
+
+    def train(self, terminal_state, step):
+        if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
+            return
+        
+        minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
+
+        current_states = np.array([transition[0] for transition in minibatch]) / 255    # by dividing here, we are trying to scale the images
+                                                                                        # between 0 and 1 because that is the best way to 
+                                                                                        # teach convolutional neural networks
+        current_qs_list = self.model.predict(current_states)
+
+        new_current_states = np.array([transition[3] for transition in minibatch]) / 255    # current states after actions are taken
+        future_qs_list = self.target_model.predict(new_current_states)
+
+        X = []      # this list will be the images from the game
+        y = []      # this list will be the actions that model decides to take
+
+        '''
+        With the following loop, we will be able to calculate the last bit (learned value)
+        of the Q-value formula
+        '''
+        for index, (current_state, action, reward, new_current_state, done) in enumerate(minibatch):
+            if not done:
+                max_future_q = np.max(future_qs_list[index])
+                new_q = reward + DISCOUNT * max_future_q
+            else:
+                new_q = reward
+
+            current_qs = current_qs_list[index]
+            current_qs[action] = new_q
+
+            X.append(current_state)
+            y.append(current_qs)
+
+        self.model.fit(np.array(X) / 255, np.array(y), batch_size = MINIBATCH_SIZE, verbose = 0, shuffle = False,
+        callbacks = [self.tensorboard] if terminal_state else None)     # this command means that we will fit onlt if we are on the 
+                                                                        # terminal state
+        
+        '''
+        Following statement determines if we want to update the target_model yet
+        '''
+        if terminal_state:
+            self.target_update_counter += 1
+        
+        if self.target_update_counter > UPDATE_TARGET_EVERY:
+            self.target_model.set_weights(self.model.get_weights())
+            self.target_update_counter = 0
+
+# Create agent
+agent = DQNAgent()
+
+'''
+Now we are ready to iterate over everything
+'''
+for episode in tqdm(range(1, EPISODES + 1), ascii = True, unit = "episode"):
+    agent.tensorboard.step = episode
+    
+    episode_reward = 0
+    step = 1
+    current_state = env.reset()
+
+    done = False
+
+    while not done:
+        if np.random.random() > epsilon:
+            action = np.argmax(agent.get_qs(current_state))
+        else:
+            action = np.random.randint(0, env.ACTION_SPACE_SIZE)
+
+        new_state, reward, done = env.step(action)
+
+        episode_reward += reward
+
+        if SHOW_PREVIEW and not episode % AGGREGATE_STATS_EVERY:
+            env.render()
+
+        agent.update_replay_memory((current_state, action, reward, new_state, done))
+        agent.train(done, step)
+
+        current_state = new_state
+        step += 1
+
+    '''
+    Now we are going to append episode reward and then we will grab various aggregate stats.
+    Then we will create a matplotlib chart with those values.
+    '''
+    # Append episode reward to a list and log stats (every given number of episodes)
+    ep_rewards.append(episode_reward)
+    if not episode % AGGREGATE_STATS_EVERY or episode == 1:
+        average_reward = sum(ep_rewards[-AGGREGATE_STATS_EVERY:])/len(ep_rewards[-AGGREGATE_STATS_EVERY:])
+        min_reward = min(ep_rewards[-AGGREGATE_STATS_EVERY:])
+        max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
+        agent.tensorboard.update_stats(reward_avg=average_reward, reward_min=min_reward, reward_max=max_reward, epsilon=epsilon)
+
+        # Save model, but only when min reward is greater or equal a set value
+        if min_reward >= MIN_REWARD:
+            agent.model.save(f'models/{MODEL_NAME}__{max_reward:_>7.2f}max_{average_reward:_>7.2f}avg_{min_reward:_>7.2f}min__{int(time.time())}.model')
+
+    # Decay epsilon
+    if epsilon > MIN_EPSILON:
+        epsilon *= EPSILON_DECAY
+        epsilon = max(MIN_EPSILON, epsilon)
